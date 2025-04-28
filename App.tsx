7e48206d05dab9a2c73938a16a84bdb7717aca09
Jigar Makwana <jigararvindbhai.makwana@maybank.com>
2025-04-28 23:06:53 +0800

    navigation

--- App.tsx
+++ App.tsx
@@ -2,37 +2,13 @@ import 'react-native-get-random-values';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaView } from 'react-native';
-import SearchScreen from './src/screens/SearchScreen';
-import HistoryScreen from './src/screens/HistoryScreen'; 
-import Ionicons from 'react-native-vector-icons/Ionicons';
-
-const Tab = createBottomTabNavigator();
+import TabNavigator from './src/navigation/TabNavigator';
 
 export default function App() {
   return (  
     <SafeAreaView style={{ flex: 1 }}>
       <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            headerShown: false,
-            tabBarIcon: ({ color, size }) => {
-              let iconName = '';
-
-              if (route.name === 'Search') {
-                iconName = 'search-outline';
-              } else if (route.name === 'History') {
-                iconName = 'time-outline';
-              }
-
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: '#007AFF',
-            tabBarInactiveTintColor: 'gray',
-          })}
-        >
-          <Tab.Screen name="Search" component={SearchScreen} />
-          <Tab.Screen name="History" component={HistoryScreen} />
-        </Tab.Navigator>
+        <TabNavigator />
       </NavigationContainer>
     </SafeAreaView>
   );
